be833ed1453c8ced46d63f8c1ab5074636b535ec
Russell Stewart <Russell.s.stewart@gmail.com>
2014-05-23 14:23:15 -0700

    Upgraded version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='pythonpy',
-    version='0.1.1dev',
+    version='0.1.1',
     packages=['pythonpy',],
     scripts=[os.path.join('bin', 'pythonpy')],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
