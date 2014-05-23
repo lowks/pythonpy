7bd19b13a5a508f4db7bd54ac1951bafadafde6f
Russell Stewart <Russell.s.stewart@gmail.com>
2014-05-23 14:24:31 -0700

    Removed numpy and mpl default import to improve speed

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='pythonpy',
-    version='0.1.1',
+    version='0.1.2',
     packages=['pythonpy',],
     scripts=[os.path.join('bin', 'pythonpy')],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
