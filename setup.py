031dc71ea697d3b10c666e1920ba324158e891d7
Russell Stewart <Russell.s.stewart@gmail.com>
2014-06-10 11:50:56 -0700

    Added README.txt back to setup.py for 0.1.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='pythonpy',
-    version='0.1.8dev',
+    version='0.1.8',
     description='Command line utility for Python',
     scripts=[os.path.join('bin', 'pythonpy')],
     license='MIT',
