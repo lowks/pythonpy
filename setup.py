7ee3e62bd6d82476e3f76f596694d2a7059c3c97
Russell Stewart <Russell.s.stewart@gmail.com>
2014-06-10 11:30:24 -0700

    0.1.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='pythonpy',
-    version='0.1.5',
+    version='0.1.6',
     description='Command line utility for Python',
     scripts=[os.path.join('bin', 'pythonpy')],
     license='MIT',
