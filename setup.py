148b562b9107bd4a4ab801405dab62794e0c6033
Russell Stewart <Russell.s.stewart@gmail.com>
2014-06-10 11:37:39 -0700

    Fixed readme issue for 0.1.7

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import os
 
 setup(
     name='pythonpy',
-    version='0.1.6',
+    version='0.1.7',
     description='Command line utility for Python',
     scripts=[os.path.join('bin', 'pythonpy')],
     license='MIT',
-    long_description=open('README.rst').read(),
+    #long_description=open('README.rst').read(),
 )
