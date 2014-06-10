16f16b9c6a06a39af02d282d46e12edbcfac9cf9
Russell Stewart <russell.s.stewart@gmail.com>
2014-06-10 00:41:27 -0700

    Changed license to MIT.

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     version='0.1.5',
     description='Command line utility for Python',
     scripts=[os.path.join('bin', 'pythonpy')],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    license='MIT',
     long_description=open('README.rst').read(),
 )
